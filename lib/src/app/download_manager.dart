ee085c93194f62702eccef3bca753af95ade398a
Arto Bendiken <arto@bendiken.net>
2018-11-15 11:19:17 +0200

    Defined android_app.DownloadManager.maxBytesOverMobile, etc.

--- lib/src/app/download_manager.dart
+++ lib/src/app/download_manager.dart
@@ -1,8 +1,35 @@
 /* This is free and unencumbered software released into the public domain. */
 
+import 'dart:async' show Future;
+
+import 'package:flutter/services.dart' show MethodChannel;
+import 'package:platform/platform.dart' show Platform, LocalPlatform;
+
+const Platform _platform = LocalPlatform();
+
 /// The download manager is a system service that handles long-running HTTP
 /// downloads.
 ///
 /// See: https://developer.android.com/reference/android/app/DownloadManager
 class DownloadManager {
+  static const MethodChannel _channel = MethodChannel('flutter_android/DownloadManager');
+
+  /// Returns maximum size, in bytes, of downloads that may go over a mobile
+  /// connection; or `null` if there's no limit.
+  ///
+  /// See: https://developer.android.com/reference/android/app/DownloadManager.html#getMaxBytesOverMobile(android.content.Context)
+  static Future<int> get maxBytesOverMobile async {
+    assert(_platform.isAndroid);
+    return await _channel.invokeMethod('getMaxBytesOverMobile') as int; // TODO: implement backend
+  }
+
+  /// Returns recommended maximum size, in bytes, of downloads that may go over
+  /// a mobile connection; or `null` if there's no recommended limit. The user
+  /// will have the option to bypass this limit.
+  ///
+  /// See: https://developer.android.com/reference/android/app/DownloadManager.html#getRecommendedMaxBytesOverMobile(android.content.Context)
+  static Future<int> get recommendedMaxBytesOverMobile async {
+    assert(_platform.isAndroid);
+    return await _channel.invokeMethod('getRecommendedMaxBytesOverMobile') as int; // TODO: implement backend
+  }
 }
