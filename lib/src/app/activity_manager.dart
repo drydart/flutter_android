bed8bf7e14a3dddf5c03c17dc32942d90b88c322
Arto Bendiken <arto@bendiken.net>
2018-11-15 11:18:45 +0200

    Defined android_app.ActivityManager.isRunningInTestHarness().

--- lib/src/app/activity_manager.dart
+++ lib/src/app/activity_manager.dart
@@ -1,8 +1,24 @@
 /* This is free and unencumbered software released into the public domain. */
 
+import 'dart:async' show Future;
+
+import 'package:flutter/services.dart' show MethodChannel;
+import 'package:platform/platform.dart' show Platform, LocalPlatform;
+
+const Platform _platform = LocalPlatform();
+
 /// Gives information about, and interacts with, activities, services, and the
 /// containing process.
 ///
 /// See: https://developer.android.com/reference/android/app/ActivityManager
 class ActivityManager {
+  static const MethodChannel _channel = MethodChannel('flutter_android/ActivityManager');
+
+  /// Returns `true` if device is running in a test harness.
+  ///
+  /// See: https://developer.android.com/reference/android/app/ActivityManager.html#isRunningInTestHarness()
+  static Future<bool> get isRunningInTestHarness async {
+    assert(_platform.isAndroid);
+    return await _channel.invokeMethod('isRunningInTestHarness') as bool; // TODO: implement backend
+  }
 }
