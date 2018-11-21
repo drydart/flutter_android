7a427f92a32b2300938a473d1563fb4182dfd396
Arto Bendiken <arto@bendiken.net>
2018-11-21 08:33:46 +0200

    Defined android_app.NotificationAction.* constants.

--- lib/src/app/notification_action.dart
+++ lib/src/app/notification_action.dart
@@ -8,4 +8,36 @@
 ///
 /// See: https://developer.android.com/reference/android/app/Notification.Action
 abstract class NotificationAction {
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_ARCHIVE
+  static const int SEMANTIC_ACTION_ARCHIVE = 5;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_CALL
+  static const int SEMANTIC_ACTION_CALL = 10;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_DELETE
+  static const int SEMANTIC_ACTION_DELETE = 4;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_MARK_AS_READ
+  static const int SEMANTIC_ACTION_MARK_AS_READ = 2;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_MARK_AS_UNREAD
+  static const int SEMANTIC_ACTION_MARK_AS_UNREAD = 3;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_MUTE
+  static const int SEMANTIC_ACTION_MUTE = 6;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_NONE
+  static const int SEMANTIC_ACTION_NONE = 0;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_REPLY
+  static const int SEMANTIC_ACTION_REPLY = 1;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_THUMBS_DOWN
+  static const int SEMANTIC_ACTION_THUMBS_DOWN = 9;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_THUMBS_UP
+  static const int SEMANTIC_ACTION_THUMBS_UP = 8;
+
+  /// See: https://developer.android.com/reference/android/app/Notification.Action.html#SEMANTIC_ACTION_UNMUTE
+  static const int SEMANTIC_ACTION_UNMUTE = 7;
 }
