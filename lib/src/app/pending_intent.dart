588f5b8015fd7ad34449d0e72356dbb6184155c8
Arto Bendiken <arto@bendiken.net>
2018-11-21 08:27:30 +0200

    Defined android_app.PendingIntent.* constants.

--- lib/src/app/pending_intent.dart
+++ lib/src/app/pending_intent.dart
@@ -4,4 +4,18 @@
 ///
 /// See: https://developer.android.com/reference/android/app/PendingIntent
 abstract class PendingIntent {
+  /// See: https://developer.android.com/reference/android/app/PendingIntent.html#FLAG_CANCEL_CURRENT
+  static const int FLAG_CANCEL_CURRENT = 0x10000000;
+
+  /// See: https://developer.android.com/reference/android/app/PendingIntent.html#FLAG_IMMUTABLE
+  static const int FLAG_IMMUTABLE = 0x04000000;
+
+  /// See: https://developer.android.com/reference/android/app/PendingIntent.html#FLAG_NO_CREATE
+  static const int FLAG_NO_CREATE = 0x20000000;
+
+  /// See: https://developer.android.com/reference/android/app/PendingIntent.html#FLAG_ONE_SHOT
+  static const int FLAG_ONE_SHOT = 0x40000000;
+
+  /// See: https://developer.android.com/reference/android/app/PendingIntent.html#FLAG_UPDATE_CURRENT
+  static const int FLAG_UPDATE_CURRENT = 0x08000000;
 }
