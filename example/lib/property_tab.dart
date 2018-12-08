e3dc788eb7f7da13fc7ca83a245c1999b8ad66c0
Arto Bendiken <arto@bendiken.net>
2018-12-08 08:22:55 +0200

    Added more properties to the example app.

--- example/lib/property_tab.dart
+++ example/lib/property_tab.dart
@@ -4,7 +4,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show PlatformException;
 import 'package:flutter_android/android.dart' show Android;
+import 'package:flutter_android/android_app.dart' as android_app;
 import 'package:flutter_android/android_content.dart' as android_content;
+import 'package:flutter_android/android_os.dart' as android_os;
 import 'package:url_launcher/url_launcher.dart';
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -13,6 +15,9 @@ typedef Future<dynamic> PropertyCallback();
 
 final Map<String, PropertyCallback> properties = <String, PropertyCallback>{
   'android.Android.platformVersion': () => Android.platformVersion,
+  'android_app.ActivityManager.isRunningInTestHarness': () => android_app.ActivityManager.isRunningInTestHarness,
+  // FIXME: 'android_app.DownloadManager.maxBytesOverMobile': () => android_app.DownloadManager.maxBytesOverMobile,
+  // FIXME: 'android_app.DownloadManager.recommendedMaxBytesOverMobile': () => android_app.DownloadManager.recommendedMaxBytesOverMobile,
   'android_content.Context.cacheDir': () => android_content.Context.cacheDir,
   'android_content.Context.codeCacheDir': () => android_content.Context.codeCacheDir,
   'android_content.Context.dataDir': () => android_content.Context.dataDir,
@@ -23,6 +28,18 @@ final Map<String, PropertyCallback> properties = <String, PropertyCallback>{
   'android_content.Context.packageCodePath': () => android_content.Context.packageCodePath,
   'android_content.Context.packageName': () => android_content.Context.packageName,
   'android_content.Context.packageResourcePath': () => android_content.Context.packageResourcePath,
+  'android_os.Build.radioVersion': () => android_os.Build.radioVersion,
+  'android_os.Build.serial': () => android_os.Build.serial,
+  'android_os.Environment.dataDirectory': () => android_os.Environment.dataDirectory,
+  'android_os.Environment.downloadCacheDirectory': () => android_os.Environment.downloadCacheDirectory,
+  'android_os.Environment.externalStorageDirectory': () => android_os.Environment.externalStorageDirectory,
+  'android_os.Environment.externalStorageState': () => android_os.Environment.externalStorageState,
+  'android_os.Environment.rootDirectory': () => android_os.Environment.rootDirectory,
+  'android_os.Environment.isExternalStorageEmulated': () => android_os.Environment.isExternalStorageEmulated,
+  'android_os.Environment.isExternalStorageRemovable': () => android_os.Environment.isExternalStorageRemovable,
+  'android_os.Process.is64Bit': () => android_os.Process.is64Bit,
+  'android_os.Process.isIsolated': () => android_os.Process.isIsolated,
+  'android_os.UserManager.supportsMultipleUsers': () => android_os.UserManager.supportsMultipleUsers,
 };
 
 ////////////////////////////////////////////////////////////////////////////////
