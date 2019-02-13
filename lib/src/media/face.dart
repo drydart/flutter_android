c733441ab56f3c14f10c6669654d5c3abc3f2589
Arto Bendiken <arto@bendiken.net>
2019-02-13 17:27:16 +0200

    Defined android_media.{Face,FaceDetector} methods.

--- lib/src/media/face.dart
+++ lib/src/media/face.dart
@@ -4,4 +4,57 @@
 /// a bitmap.
 ///
 /// See: https://developer.android.com/reference/android/media/FaceDetector.Face
-abstract class Face {}
+class Face {
+
+  /// The minimum confidence factor of good face recognition.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#CONFIDENCE_THRESHOLD
+  static const double CONFIDENCE_THRESHOLD = 0.4;
+
+  /// The X-axis Euler angle of a face.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#EULER_X
+  static const int EULER_X = 0;
+
+  /// The Y-axis Euler angle of a face.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#EULER_Y
+  static const int EULER_Y = 1;
+
+  /// The Z-axis Euler angle of a face.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#EULER_Z
+  static const int EULER_Z = 2;
+
+  /// A confidence factor between 0 and 1.
+  ///
+  /// This indicates how certain what has been found is actually a face.
+  /// A confidence factor above 0.3 is usually good enough.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#confidence()
+  final double confidence;
+
+  /// The distance between the eyes.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#eyesDistance()
+  final double eyesDistance;
+
+  Face({this.confidence, this.eyesDistance});
+
+  /// The Euler angle of the face, around the X axis.
+  double get poseX => pose(EULER_X);
+
+  /// The Euler angle of the face, around the Y axis.
+  double get poseY => pose(EULER_Y);
+
+  /// The Euler angle of the face, around the Z axis.
+  double get poseZ => pose(EULER_Z);
+
+  /// Returns the face's pose. That is, the rotations around either the X, Y or
+  /// Z axis (the positions in 3-dimensional Euclidean space).
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector.Face.html#pose(int)
+  double pose(final int euler) {
+    return null; // TODO
+  }
+}
--- lib/src/media/face_detector.dart
+++ lib/src/media/face_detector.dart
@@ -1,8 +1,39 @@
 /* This is free and unencumbered software released into the public domain. */
 
 import '../graphics/bitmap.dart' show Bitmap;
+import 'face.dart' show Face;
 
 /// Identifies the faces of people in a [Bitmap] graphic object.
 ///
 /// See: https://developer.android.com/reference/android/media/FaceDetector
-abstract class FaceDetector {}
+class FaceDetector {
+  /// The width of the images to be analyzed.
+  final int width;
+
+  /// The height of the images to be analyzed.
+  final int height;
+
+  /// The maximum number of faces to identify.
+  final int maxFaces;
+
+  /// Creates a FaceDetector, configured with the size of the images to be
+  /// analyzed and the maximum number of faces that can be detected.
+  ///
+  /// These parameters cannot be changed once the object is constructed.
+  /// Note that the width of the image must be even.
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector#FaceDetector(int,%20int,%20int)
+  FaceDetector({this.width, this.height, this.maxFaces = 1})
+      : assert(width % 2 == 0);
+
+  /// Finds all the faces found in a given [Bitmap].
+  ///
+  /// Returns an array of [Face] for each face found.
+  ///
+  /// The bitmap must be in 565 format (for now).
+  ///
+  /// See: https://developer.android.com/reference/android/media/FaceDetector#findFaces(android.graphics.Bitmap,%20android.media.FaceDetector.Face[])
+  List<Face> findFaces(final Bitmap bitmap) {
+    return <Face>[]; // TODO
+  }
+}
