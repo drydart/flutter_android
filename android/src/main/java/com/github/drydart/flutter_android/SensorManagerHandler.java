69d2b27ed6ecb239f951d69438d6f4d9cd56776d
Arto Bendiken <arto@bendiken.net>
2019-02-16 07:42:11 +0200

    Implemented android_hardware.SensorManager.getDefaultSensor().

--- android/src/main/java/com/github/drydart/flutter_android/SensorManagerHandler.java
+++ android/src/main/java/com/github/drydart/flutter_android/SensorManagerHandler.java
@@ -2,14 +2,21 @@
 
 package com.github.drydart.flutter_android;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import java.util.HashMap;
+import java.util.Map;
 
 /** SensorManagerHandler */
 @SuppressWarnings("unchecked")
 class SensorManagerHandler extends FlutterMethodCallHandler {
   static final String CHANNEL = "flutter_android/SensorManager";
+
+  final Map<Integer, Sensor> sensors = new HashMap<>();
 
   SensorManagerHandler(final Registrar registrar) {
     super(registrar);
@@ -22,7 +29,25 @@ class SensorManagerHandler extends FlutterMethodCallHandler {
 
     assert(call.method != null);
     switch (call.method) {
-      // TODO
+      /* Static methods */
+
+      case "getDefaultSensor": {
+        final int sensorType = getRequiredArgument(call, "type");
+        if (this.sensors.containsKey(sensorType)) {
+          result.success(sensorType);
+          break;
+        }
+        final Context context = registrar.context();
+        final SensorManager sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        final Sensor sensor = sensorManager.getDefaultSensor(sensorType);
+        if (sensor == null) {
+          result.success(null);
+          break;
+        }
+        this.sensors.put(sensorType, sensor);
+        result.success(sensorType);
+        break;
+      }
 
       default: {
         result.notImplemented();
--- lib/src/hardware/sensor_manager.dart
+++ lib/src/hardware/sensor_manager.dart
@@ -2,10 +2,26 @@
 
 import 'package:flutter/services.dart' show MethodChannel;
 
+import 'sensor.dart' show Sensor;
+
+const MethodChannel _channel =
+    MethodChannel('flutter_android/SensorManager');
+
 /// [SensorManager] lets you access the device's sensors.
 ///
 /// See: https://developer.android.com/reference/android/hardware/SensorManager
 abstract class SensorManager {
-  static const MethodChannel _channel =
-      MethodChannel('flutter_android/SensorManager');
+
+  /// Gets the default sensor for a given type.
+  ///
+  /// See: https://developer.android.com/reference/android/hardware/SensorManager.html#getDefaultSensor(int)
+  static Future<Sensor> getDefaultSensor(final int type) async {
+    final Map<String, dynamic> request = <String, dynamic>{'type': type};
+    final int key = await _channel.invokeMethod('getDefaultSensor', request);
+    return (key == null) ? null : Sensor(
+      key: key,
+      name: null, // TODO
+      type: type,
+    );
+  }
 }
